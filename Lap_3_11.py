3f0a1ca5bc6d958f0420c43f8004634582de22ca
agutemal <69372966+agutemal@users.noreply.github.com>
2020-12-26 21:43:42 -0500

    Add files via upload

---
+++ Lap_3_11.py
@@ -0,0 +1,8 @@
+print("Ingrese una entrada")
+entrada=input()
+if entrada=="Espatifilo":
+    print("¡Espatifilo es la mejor planta de todas!")
+elif entrada=="espatifilo":
+    print("No, ¡quiero un gran Espatifilo!")
+else:
+    print("!Espatifilo¡ !No",entrada,"!")
