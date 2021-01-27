dec00480dc5c1eb04a80d769c1fd8d5b6b018916
agutemal <69372966+agutemal@users.noreply.github.com>
2021-01-27 16:50:29 -0500

    Add files via upload

---
+++ Lab_3143 Listas.py
@@ -0,0 +1,32 @@
+﻿# paso 1
+beatles=[]
+print("Paso 1:", beatles)
+
+# paso 2
+
+beatles.append("John Lennon")
+beatles.append("Paul McCartney")
+beatles.append("George Harrison")
+print("Paso 2:", beatles)
+# paso 3
+
+for i in range(2):
+    if i==0:
+        beatles.append(input("Inprese el nombre de Stu Sutcliffe: "))
+    else:
+        beatles.append(input("Ingrese el nombre de Pete Best: "))
+print("Paso 3:", beatles)        
+# etapa 4
+
+beatles.remove("Stu Sutcliffe")
+beatles.remove("Pete Best")
+print("Paso 4:", beatles)
+
+# paso 5
+beatles.insert(0,"Ringo Starr")
+print("Paso 5:", beatles)
+
+
+
+# probando la longitud de la lista
+print("Los Fab", len(beatles))
